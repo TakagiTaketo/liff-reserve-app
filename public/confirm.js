790d795980b666b3b9dba9fe8b62f97f1647ff35
takagi@test <taketo19960513@gmail.com>
2023-06-09 14:12:13 +0900

    予約取消作成中

--- public/confirm.js
+++ public/confirm.js
@@ -5,10 +5,63 @@
 
 
 let result = [];
-document.addEventListener("DOMContentLoaded", function (event) {
-    // 予約管理DBから予約情報を取得
-    select_reserves();
+let line_uid = '';
+let line_uname = '';
+document.addEventListener("DOMContentLoaded", () => {
+    const liffId = '1661289930-qLmEmZ8w';
+    // LIFF 初期化
+    liff.init({
+        liffId: liffId
+    })
+        .then(() => {
+            initializeApp();
+            const idtoken = liff.getIDToken();
+            const jsonData = JSON.stringify({
+                id_token: idtoken
+            });
+            // LINEプロフィール取得
+            fetch('/api', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: jsonData,
+                creadentials: 'same-origin'
+            })
+                .then(res => {
+                    res.json()
+                        .then(json => {
+                            console.log('json:' + json);
+                            line_uname = json.line_uname;
+                            line_uid = json.line_uid;
+                        })
+
+                    // 予約管理DBから予約情報を取得
+                    select_reserves();
+                })
+                .catch((err) => {
+                    alert(err);
+                })
+        })
+        .catch((err) => {
+            alert(err);
+        })
+
+
 });
+
+function initializeApp() {
+    // ログインチェック
+    if (liff.isLoggedIn()) {
+        //ログイン済
+    } else {
+        // 未ログイン
+        let result = window.confirm("LINE Loginを行います。");
+        if (result) {
+            liff.login();
+        }
+    }
+}
 
 // 予約情報取得
 async function select_reserves() {
