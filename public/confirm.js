5bbcbcf6af8e71f1339d8df26442d5bda5936369
takagi@test <taketo19960513@gmail.com>
2023-06-12 14:39:03 +0900

    取り消し処理作成中

--- public/confirm.js
+++ public/confirm.js
@@ -221,37 +221,20 @@ async function select_reserves() {
 */
 // 予約取消ボタン
 async function deleteReserve() {
-    let reserveDate = document.getElementsByName('checkbox');
-    let dateList = document.getElementsByName('date');
-    let startList = document.getElementsByName('start');
-    let hiddenDateList = document.getElementsByName('hidden_date');
+    // セッションから選択した予約でデータの日付を取得する
+    let jsonData = sessionStorage.getItem('jsonData');
+    jsonData = JSON.parse(jsonData);
 
-    let jsonData = [];
-    let check_flg = false;
-    for (let i = 0; i < reserveDate.length; i++) {
-        if (reserveDate[i].checked) {
-            let addData = { reserveDate: dateList[i].innerText, reserve_time: startList[i].innerText, line_uid: line_uid }
-            jsonData.push(addData);
-            check_flg = true;
-        }
-    }
-    if (!check_flg) {
-        alert('取り消す予約情報を選択してください。');
-        return false;
-    } else {
-        console.log('取り消し処理 JSON:' + JSON.stringify(jsonData));
-        jsonData = JSON.stringify(jsonData);
-        // TODO 取消は未確認。
-        await fetch('/updateReserve', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin',
-            body: jsonData
-        })
-        let msg = '予約取消' + "\n" + checked_date;
-        sendText(msg);
-        return false;
-    }
+    // TODO 取消は未確認。
+    await fetch('/updateReserve', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(jsonData)
+    })
+    let msg = '予約を取り消しました。';
+    sendText(msg);
+    return false;
 }
