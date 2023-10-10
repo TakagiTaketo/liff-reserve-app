b99541dafb7a5e35d8580189692f14121da81611
TakagiTaketo <takagi_taketo@medi-brain.com>
2023-10-10 10:26:51 +0900

    トランザクション処理を追加

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ const replyMessage = (req, res) => {
 const insertReserve = async (req, res) => {
   const data = req.body;
   const idToken = data.idToken; // IDトークンを取得
+  const client = await pool.connect();  // 接続を手動取得
   try {
     const userInfo = await verifyIdTokenAndGetUserInfo(idToken); // IDトークンを検証し、ユーザー情報を取得
 
@@ -147,33 +148,19 @@ const insertReserve = async (req, res) => {
       Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000
     );
 
-    created_at =
-      date.getFullYear() +
-      "/" +
-      ("0" + (date.getMonth() + 1)).slice(-2) +
-      "/" +
-      ("0" + date.getDate()).slice(-2) +
-      " " +
-      ("0" + date.getHours()).slice(-2) +
-      ":" +
-      ("0" + date.getMinutes()).slice(-2) +
-      ":" +
-      ("0" + date.getSeconds()).slice(-2);
+    created_at = date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + ("0" + date.getDate()).slice(-2) +
+      " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) +
+      ":" + ("0" + date.getSeconds()).slice(-2);
 
     // 予約フォームに登録済みかどうか確認する
     const check_query1 = {
       text: `SELECT * FROM reserves WHERE line_uid=$1 AND delete_flg=0;`,
-      values: [userInfo.line_uid],
+      values: [userInfo.line_uid]
     };
-    const existingReserve1 = await pool.query(check_query1);
+    const existingReserve1 = await client.query(check_query1);
     if (existingReserve1.rowCount > 0) {
       // 既にその日時での予約が存在する場合
-      res
-        .status(400)
-        .send({
-          error:
-            "面談の予約は初回のみ行うことができます。\n2回目以降の面談をご希望の場合はトークルームでその旨をお伝えください。",
-        });
+      res.status(400).send({ error: "面談の予約は初回のみ行うことができます。\n2回目以降の面談をご希望の場合はトークルームでその旨をお伝えください。"});
       console.log("面談の予約は初回のみ");
       return;
     }
@@ -181,18 +168,13 @@ const insertReserve = async (req, res) => {
     // 指定された日時での既存の予約を確認
     const check_query2 = {
       text: `SELECT * FROM reserves WHERE reserve_date = $1 AND reserve_time = $2 AND delete_flg = 0`,
-      values: [data.reserve_date, data.reserve_time],
+      values: [data.reserve_date, data.reserve_time]
     };
 
-    const existingReserve2 = await pool.query(check_query2);
+    const existingReserve2 = await client.query(check_query2);
     if (existingReserve2.rowCount > 0) {
       // 既にその日時での予約が存在する場合
-      res
-        .status(400)
-        .send({
-          error:
-            "選択していただいた日時は満席のため、予約出来ませんでした。\n最新の状態を確認するには更新してください。",
-        });
+      res.status(400).send({ error: "選択していただいた日時は満席のため、予約出来ませんでした。\n最新の状態を確認するには更新してください。"});
       console.log("満席のため予約出来ませんでした");
       return;
     }
@@ -202,55 +184,28 @@ const insertReserve = async (req, res) => {
       text: `SELECT * FROM no_reserves WHERE no_reserve_date=$1 and no_reserve_time=$2 and delete_flg=0;`,
       values: [data.reserve_date, data.reserve_time],
     };
-    const existingReserve3 = await pool.query(check_query3);
+    const existingReserve3 = await client.query(check_query3);
     if (existingReserve3.rowCount > 0) {
       // 予約不可日の場合
-      res
-        .status(400)
-        .send({
-          error:
-            "選択していただいた日時は休診のため、予約出来ませんでした。\n最新の状態を確認するには更新してください。",
-        });
+      res.status(400).send({ error: "選択していただいた日時は休診のため、予約出来ませんでした。\n最新の状態を確認するには更新してください。"});
       console.log("休診のため予約出来ませんでした。");
       return;
     }
 
     const insert_query = {
       text: `INSERT INTO reserves(line_uid, name, reserve_date, reserve_time, created_at, delete_flg, birthday) VALUES ($1, $2, $3, $4, $5, $6, $7);`,
-      values: [
-        userInfo.line_uid,
-        data.name,
-        data.reserve_date,
-        data.reserve_time,
-        created_at,
-        0,
-        data.birthday,
-      ],
+      values: [userInfo.line_uid, data.name, data.reserve_date, data.reserve_time, created_at, 0, data.birthday]
     };
 
-    pool
-      .query(insert_query)
-      .then(() => {
-        let message = "予約追加完了";
-        res.status(200).send({ msg: message });
-      })
-      .catch((e) => {
-        console.error(e.message);
-        res
-          .status(500)
-          .send({
-            error:
-              "予約に失敗しました。\n一度アプリを閉じて再度お試しください。",
-          });
-      });
+    await client.query(insert_query);
+    await client.query('COMMIT'); // トランザクションの確定
+    res.status(200).send({ msg: "予約追加完了" });
   } catch (e) {
+    await client.query('ROLLBACK'); // エラーが発生した場合、トランザクションをロールバックする。
     console.error(e.message);
-    res
-      .status(500)
-      .send({
-        error:
-          "何らかの問題が発生し、予約に失敗しました。\n一度アプリを閉じて再度お試しください。",
-      });
+    res.status(500).send({ error: "何らかの問題が発生し、予約に失敗しました。\n一度アプリを閉じて再度お試しください。"});
+  } finally {
+    client.release(); // 接続をプールに返却する
   }
 };
 
@@ -262,14 +217,13 @@ const selectWeekReserve = (req, res) => {
   // SELECT文
   const select_query = {
     text: `SELECT name, reserve_date, reserve_time FROM reserves WHERE delete_flg=0 AND reserve_date BETWEEN $1 AND $2 ORDER BY reserve_date ASC, reserve_time ASC;`,
-    values: [startDate, endDate],
+    values: [startDate, endDate]
   };
   let dataList = [];
 
   // SQL実行
-  pool
-    .query(select_query)
-    .then((data) => {
+  pool.query(select_query)
+  .then((data) => {
       for (let i = 0; i < data.rows.length; i++) {
         let tmp_data = {};
         tmp_data.name = data.rows[i].name;
@@ -282,12 +236,7 @@ const selectWeekReserve = (req, res) => {
     })
     .catch((e) => {
       console.error(e.message);
-      res
-        .status(500)
-        .send({
-          error:
-            "予約カレンダーの取得に失敗しました。\n一度アプリを閉じて再度開いてください。",
-        });
+      res.status(500).send({ error: "予約カレンダーの取得に失敗しました。\n一度アプリを閉じて再度開いてください。"});
     });
 };
 
@@ -299,11 +248,10 @@ const selectNoReserve = (req, res) => {
 
   const select_query = {
     text: `SELECT name, no_reserve_date, no_reserve_time FROM no_reserves WHERE delete_flg=0 AND no_reserve_date BETWEEN $1 AND $2 ORDER BY no_reserve_date ASC, no_reserve_time ASC;`,
-    values: [startDate, endDate],
+    values: [startDate, endDate]
   };
   let dataList = [];
-  pool
-    .query(select_query)
+  pool.query(select_query)
     .then((data) => {
       for (let i = 0; i < data.rows.length; i++) {
         let tmp_data = {};
@@ -312,19 +260,12 @@ const selectNoReserve = (req, res) => {
         tmp_data.no_reserve_time = data.rows[i].no_reserve_time;
         dataList.push(tmp_data);
       }
-      console.log(
-        "サーバーサイドselectNoReserve()のdataList:" + JSON.stringify(dataList)
-      );
+      console.log("サーバーサイドselectNoReserve()のdataList:" + JSON.stringify(dataList));
       res.status(200).send(JSON.stringify(dataList));
     })
     .catch((e) => {
       console.error(e.message);
-      res
-        .status(500)
-        .send({
-          error:
-            "休診カレンダーの取得に失敗しました。\n一度アプリを閉じて再度開いてください。",
-        });
+      res.status(500).send({ error: "休診カレンダーの取得に失敗しました。\n一度アプリを閉じて再度開いてください。"});
     });
 };
 // 予約確認データ取得
@@ -340,8 +281,7 @@ const selectConfirmReserve = async (req, res) => {
       values: [userInfo.line_uid],
     };
     let dataList = [];
-    pool
-      .query(select_query)
+    await pool.query(select_query)
       .then((data) => {
         for (let i = 0; i < data.rows.length; i++) {
           let tmp_data = {};
@@ -353,21 +293,11 @@ const selectConfirmReserve = async (req, res) => {
       })
       .catch((e) => {
         console.error(e.message);
-        res
-          .status(500)
-          .send({
-            error:
-              "予約データの取得に失敗しました。\n一度アプリを閉じて再度お試しください。",
-          });
+        res.status(500).send({ error: "予約データの取得に失敗しました。\n一度アプリを閉じて再度お試しください。"});
       });
   } catch (e) {
     console.error(e.message);
-    res
-      .status(500)
-      .send({
-        error:
-          "何らかの問題が発生し、予約データの取得に失敗しました。\n一度アプリを閉じて再度お試しください。",
-      });
+    res.status(500).send({ error: "何らかの問題が発生し、予約データの取得に失敗しました。\n一度アプリを閉じて再度お試しください。"});
   }
 };
 
@@ -381,60 +311,37 @@ const updateReserve = async (req, res) => {
 
     // タイムスタンプ整形
     let updated_at = "";
-    let date = new Date(
-      Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000
-    );
-    updated_at =
-      date.getFullYear() +
-      "/" +
-      ("0" + (date.getMonth() + 1)).slice(-2) +
-      "/" +
-      ("0" + date.getDate()).slice(-2) +
-      " " +
-      ("0" + date.getHours()).slice(-2) +
-      ":" +
-      ("0" + date.getMinutes()).slice(-2) +
-      ":" +
-      ("0" + date.getSeconds()).slice(-2);
+    let date = new Date(Date.now() + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000);
+    updated_at = date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2) +
+      "/" + ("0" + date.getDate()).slice(-2) +
+      " " + ("0" + date.getHours()).slice(-2) +
+      ":" + ("0" + date.getMinutes()).slice(-2) +
+      ":" + ("0" + date.getSeconds()).slice(-2);
 
     const line_uid = userInfo.line_uid;
-    const reserve_date =
-      data.reserve_date.substring(0, 4) +
-      "-" +
-      data.reserve_date.substring(5, 7) +
-      "-" +
-      data.reserve_date.substring(8, 10);
+    const reserve_date = data.reserve_date.substring(0, 4) +
+      "-" + data.reserve_date.substring(5, 7) +
+      "-" + data.reserve_date.substring(8, 10);
     const reserve_time = data.reserve_time;
     console.log("updateReserve()のline_uid:" + line_uid);
     console.log("updateReserve()のreserve_date:" + reserve_date);
     console.log("updateReserve()のreserve_time:" + reserve_time);
     const update_query = {
       text: `UPDATE reserves set updated_at=$1, delete_flg=1 WHERE line_uid=$2 AND reserve_date=$3 AND reserve_time=$4;`,
-      values: [updated_at, line_uid, reserve_date, reserve_time],
+      values: [updated_at, line_uid, reserve_date, reserve_time]
     };
 
-    await pool
-      .query(update_query)
+    await pool.query(update_query)
       .then(() => {
         res.status(200).send({ msg: "予約を取り消しました。" });
       })
       .catch((e) => {
         console.error(e.message);
-        res
-          .status(500)
-          .send({
-            error:
-              "予約の取り消しに失敗しました。\n一度アプリを閉じて再度お試しください。",
-          });
+        res.status(500).send({ error: "予約の取り消しに失敗しました。\n一度アプリを閉じて再度お試しください。"});
       });
   } catch (e) {
     console.log(e.message);
-    res
-      .status(500)
-      .send({
-        error:
-          "予約の取り消しに失敗しました。\n一度アプリを閉じて再度お試しください。",
-      });
+    res.status(500).send({ error: "予約の取り消しに失敗しました。\n一度アプリを閉じて再度お試しください。"});
   }
 };
 
@@ -453,28 +360,23 @@ const sendEmail = async (req, res) => {
       port: 587, // SMTPサーバーのポート
       secure: false, // SSL/TLSを使用しない場合はfalse
       auth: {
-        user: "takagi_taketo@medi-brain.com", // あなたのメールアドレス
-        pass: "Tak_tak221115", // あなたのメールアカウントのパスワード
+        user: "takagi_taketo@medi-brain.com", // 送信するメールアドレス
+        pass: "Tak_tak221115", // パスワード
       },
     };
 
     // Nodemailerのトランスポートを作成
     const transporter = nodemailer.createTransport(smtpConfig);
 
-    let mail_text =
-      userInfo.line_uname +
-      "　さんが面談予約しました。\n面談者名：" +
-      data.reserve_name +
-      "\n対象日時：" +
-      data.reserve_date +
-      "　" +
-      data.reserve_time;
+    let mail_text = userInfo.line_uname + "　さんが面談予約しました。\n面談者名：" +
+      data.reserve_name + "\n対象日時：" +
+      data.reserve_date + "　" + data.reserve_time;
     // HTMLメールの本文に改行を反映
     let mail_html = mail_text.replace(/\n/g, "<br>");
     // メールの内容
     const mailOptions = {
-      from: "takagi_taketo@medi-brain.com", // 送信者のアドレス
-      to: 'hoken_moriguchi@medi-brain.com', // 受信者のアドレス
+      from: "takagi_taketo@medi-brain.com", // 送信者のメールアドレス
+      to: 'hoken_moriguchi@medi-brain.com', // 受信者のメールアドレス
       subject: "【予約】守口　保健指導", // 件名
       text: mail_text, // テキスト本文
       html: "<p>" + mail_html + "</p>", // HTML本文
@@ -509,10 +411,10 @@ const verifyIdTokenAndGetUserInfo = async (idToken) => {
     const data = await response.json();
     return {
       line_uid: data.sub,
-      line_uname: data.name,
+      line_uname: data.name
     };
   } catch (e) {
     console.error(e.message);
-    throw new Error("Failed to verify ID token or fetch user info");
+    throw new Error("ユーザー情報の取得に失敗しました。");
   }
 };
